f07cc54659fc209cce11df21b14c9f3cdef33886
Minguk Choi <koreachoi96@gmail.com>
2023-04-08 20:39:15 +0900

    Correct training and prediction errors
    Correct training and prediction errors

--- include/rs/radix_spline.h
+++ include/rs/radix_spline.h
@@ -53,11 +53,11 @@ class RadixSpline {
   // Returns a search bound [begin, end) around the estimated position.
   SearchBound GetSearchBound(const KeyType key) const {
     const size_t estimate = GetEstimatedPosition(key);
-    const size_t begin = (estimate < max_error_ - 1) ? 0 : (estimate - max_error_ + 1);
+    const size_t begin = (estimate < max_error_) ? 0 : (estimate - max_error_);
     // `end` is exclusive.
-    const size_t end = (estimate + max_error_ + 2 > num_keys_)
+    const size_t end = (estimate + max_error_ + 3 > num_keys_)
                            ? num_keys_
-                           : (estimate + max_error_ + 2);
+                           : (estimate + max_error_ + 3);
     return SearchBound{begin, end};
   }
 
